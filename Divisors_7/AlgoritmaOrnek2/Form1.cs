f57116e67ddf3517b3bbb3b1ba2a0e07fd3f41bd
ElifHangul <elifhangul96@gmail.com>
2022-01-21 19:02:37 +0300

    adding the code

---
+++ Divisors_7/AlgoritmaOrnek2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoritmaOrnek2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int toplam = 0;
+        int bolunenSayisi = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    bolunenSayisi++;
+                    label3.Text = bolunenSayisi.ToString();
+                    toplam += i;
+                    label4.Text = toplam.ToString();
+                }
+
+            }
+        }
+    }
+}
